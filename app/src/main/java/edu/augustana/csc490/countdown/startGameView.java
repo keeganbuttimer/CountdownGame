10526d312c831c00f6a6b1c078e9c883f0d99515
keeganbuttimer11 <keeganbuttimer11@augustana.edu>
2015-04-09 15:36:23 -0500

    Multiple "Levels", Fixed an error in checking if the circles overlapped (radius*2 instead of just radius)

--- app/src/main/java/edu/augustana/csc490/countdown/startGameView.java
+++ app/src/main/java/edu/augustana/csc490/countdown/startGameView.java
@@ -15,6 +15,8 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.Random;
+
 public class startGameView extends SurfaceView implements SurfaceHolder.Callback
 {
     private static final String TAG = "GameStarter"; // for Log.w(TAG, ...)
@@ -26,19 +28,30 @@ public class startGameView extends SurfaceView implements SurfaceHolder.Callback
 
     private int centerX;
     private int centerY;
+    private int trackRadius;
+
     private double circle1x;
     private double circle1y;
     private int circle1radius;
+    private double circle1degree;
+    private int circle1speed;
+
     private double circle2x;
     private double circle2y;
     private int circle2radius;
-    private double circle1degree;
     private double circle2degree;
+    private int circle2speed;
+
     private int screenWidth;
     private int screenHeight;
     private Paint circleBackgroundPaint;
     private Paint backgroundPaint;
     private Paint circlePaint;
+    private Paint pass;
+    private Paint fail;
+    private Random r;
+
+    private boolean changeColor;
 
     public startGameView(Context context, AttributeSet atts)
     {
@@ -52,6 +65,10 @@ public class startGameView extends SurfaceView implements SurfaceHolder.Callback
         backgroundPaint = new Paint();
         backgroundPaint.setColor(Color.parseColor("#BEC0C0"));
         circlePaint = new Paint();
+        pass=new Paint();
+        pass.setColor(Color.GREEN);
+        fail=new Paint();
+        fail.setColor(Color.RED);
         circlePaint.setColor(Color.DKGRAY);
     }
 
@@ -63,10 +80,13 @@ public class startGameView extends SurfaceView implements SurfaceHolder.Callback
 
         screenWidth = w;
         screenHeight = h;
+        trackRadius=100;
         circle1degree=0;
         circle2degree=0;
-        circle1radius=10;
-        circle2radius=10;
+        circle1radius=40;
+        circle2radius=40;
+        circle1speed=2;
+        circle2speed=2;
         startNewGame();
     }
 
@@ -74,8 +94,9 @@ public class startGameView extends SurfaceView implements SurfaceHolder.Callback
     {
         centerX = screenWidth/2;
         centerY = screenHeight/2;
-        circle1x= centerX+200;
+        circle1x= centerX+trackRadius;
         circle1y= centerY;
+        r= new Random();
         if (isGameOver)
         {
             isGameOver = false;
@@ -87,28 +108,38 @@ public class startGameView extends SurfaceView implements SurfaceHolder.Callback
 
     private void gameStep()
     {
-        moveCircleOne();
+        moveCircleOne(circle1speed,circle2speed);
     }
-    public void moveCircleOne(){
-        circle1degree+=Math.PI/180;
-        circle2degree-=2*Math.PI/180;
-        circle1x= (centerX+(200*Math.cos(circle1degree)));
-        circle1y= (centerY+(200*Math.sin(circle1degree)));
-        circle2x= (centerX+(200*Math.cos(circle2degree)));
-        circle2y= (centerY+(200*Math.sin(circle2degree)));
+    public void moveCircleOne(int speed1,int speed2){
+        circle1degree+=speed1*Math.PI/180;
+        circle2degree-=speed2*Math.PI/180;
+        circle1x= (centerX+(trackRadius*Math.cos(circle1degree)));
+        circle1y= (centerY+(trackRadius*Math.sin(circle1degree)));
+        circle2x= (centerX+(trackRadius*Math.cos(circle2degree)));
+        circle2y= (centerY+(trackRadius*Math.sin(circle2degree)));
     }
     public void updateView(Canvas canvas)
     {
 
         if (canvas != null) {
             canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), backgroundPaint);
-            canvas.drawCircle(centerX, centerY, 200, circleBackgroundPaint);
+            if(changeColor){
+                if(displayResult()==1) {
+                    canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), pass);
+                }
+                else{
+                    canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), fail);
+                }
+            }
+            canvas.drawCircle(centerX, centerY, trackRadius, circleBackgroundPaint);
             canvas.drawCircle((int)circle1x,(int)circle1y,circle1radius, circlePaint);
             canvas.drawCircle((int)circle2x,(int)circle2y,circle2radius, circlePaint);
-
-           if(isGameOver) {
-               canvas.drawText(displayResult(), 200, 200, circlePaint);
-           }
+            canvas.drawText("Circle 1 Speed: " + circle1speed, 200, 140, circlePaint);
+            canvas.drawText("Circle 2 Speed: "+circle2speed,200,170,circlePaint);
+            canvas.drawText("Circle 1 Radius: "+circle1radius,200,200,circlePaint);
+            canvas.drawText("Circle 2 Radius: "+circle2radius,200,230,circlePaint);
+            canvas.drawText("Track radius: "+trackRadius,200,260,circlePaint);
+            changeColor=false;
 
 
 
@@ -165,17 +196,45 @@ public class startGameView extends SurfaceView implements SurfaceHolder.Callback
     {
         if (e.getAction() == MotionEvent.ACTION_DOWN)
         {
-            isGameOver=true;
+            changeColor=true;
+            if(displayResult()==0) {
+                //isGameOver = true;
+            }
+            else{
+                trackRadius+=50;
+                if(trackRadius>400){
+                    trackRadius=100;
+                    speedUp();
+                    if(circle1speed>5 || circle2speed>5) {
+                        circle1speed=2;
+                        circle2speed=2;
+                        shrink();
+                        if(circle1radius<20 || circle2radius<20){
+                            isGameOver=true;
+                        }
+                    }
+                }
+            }
         }
 
         return true;
     }
-
-    public String displayResult(){
-        if(Math.abs(circle1x - circle2x)<circle1radius && Math.abs(circle1y-circle2y)<circle1radius){
-            return "Victory!";
+    public void shrink(){
+        circle1radius-=5;
+        circle2radius-=5;
+    }
+    public void speedUp(){
+        circle1speed++;
+        circle2speed++;
+    }
+    public int displayResult(){
+        Paint result= new Paint();
+        result.setColor(Color.RED);
+        if(Math.abs(circle1x - circle2x)<circle1radius*2 && Math.abs(circle1y-circle2y)<circle1radius*2){
+           result.setColor(Color.GREEN);
+            return 1;
         }
-        return "Failure!";
+        return 0;
     }
     // Thread subclass to run the main game loop
     private class GameThread extends Thread
@@ -216,7 +275,7 @@ public class startGameView extends SurfaceView implements SurfaceHolder.Callback
                         }
                         updateView(canvas); // draw using the canvas
                     }
-                   Thread.sleep(10); // if you want to slow down the action...
+                   Thread.sleep(1); // if you want to slow down the action...
                 } catch (InterruptedException ex) {
                     Log.e(TAG,ex.toString());
                 }
